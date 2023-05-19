6f540a8a0f3e35841832d21fbbd71c2898774256
Jet <jetpoke1011@gmail.com>
2023-05-19 14:01:28 -0400

    initialize values

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,31 @@
 class PolyTreeNode
-  
+
+    def initialize(value)
+        @parent = nil 
+        @children = []
+        @value = value  
+    end 
+
+    attr_reader :parent, :children, :value 
+
+
+    def parent=(new_node)
+        if self.parent == nil
+            new_node = parent
+        end 
+
+        if self.parent != nil
+            self.children << new_node 
+        end 
+
+
+        # new_node.children.push(new_node.children.children)
+
+
+
+    end 
+
+
+
+
 end
